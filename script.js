5f7f3564c0cea46fde4064e0625be322d142c34f
michael henrys <michael42h@gmail.com>
2024-09-01 14:34:34 +1200

    add gitignore and add bundler

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 import * as THREE from 'three';
+import { GLTFLoader } from 'three/examples/jsm/Addons.js';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
@@ -7,9 +8,13 @@ const renderer = new THREE.WebGLRenderer({ canvas: document.querySelector('#canv
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-const geometry = new THREE.BoxGeometry();
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-const cube = new THREE.Mesh(geometry, material);
+const loader = new GLTFLoader();
+
+loader.load( 'path/to/model.glb', function ( gltf ) {
+	scene.add( gltf.scene );
+}, undefined, function ( error ) {
+	console.error( error );
+} );
 scene.add(cube);
 
 camera.position.z = 5;
