459467b0c0e0aec70d27fa81b15ff323205a8452
michael henrys <michael42h@gmail.com>
2024-09-12 21:52:29 +1200

    Improve light coloring

--- script.js
+++ script.js
@@ -14,20 +14,24 @@ document.body.appendChild(renderer.domElement);
 scene.background = new THREE.Color(0xffffff);
 
 
-// // Add blue fog to simulate a glow effect
-// scene.fog = new THREE.Fog(0x87CEEB, 1, 900); 
+// Add blue fog to simulate a glow effect
+scene.fog = new THREE.Fog(0x87CEEB, 1, 1700); 
 
-const directionalLight = new THREE.DirectionalLight(0xffffff, 10);
+const directionalLight = new THREE.DirectionalLight(0xffffff, 20);
 directionalLight.position.set(0, -2, 0.5).normalize();
 scene.add(directionalLight);
 
-const directionalLight2 = new THREE.DirectionalLight(0xffffff, 10);
+const directionalLight2 = new THREE.DirectionalLight(0xf700ff, 5);
 directionalLight2.position.set(-2, 0, 0).normalize();
 scene.add(directionalLight2);
 
-const directionalLight3 = new THREE.DirectionalLight(0xffffff, 10);
-directionalLight3.position.set(2, 0, 2).normalize();
+const directionalLight3 = new THREE.DirectionalLight(0x00c9ff, 10);
+directionalLight3.position.set(-2, 0, 0).normalize();
 scene.add(directionalLight3);
+
+const directionalLight4 = new THREE.DirectionalLight(0x00c9ff, 10);
+directionalLight4.position.set(2, 1, 2).normalize();
+scene.add(directionalLight4);
 
 
 
@@ -38,8 +42,8 @@ loader.load('/sigil/sigil.glb', function (gltf) {
     gltf.scene.scale.set(1, 1, 1); // Adjust scale if needed
     gltf.scene.traverse((child) => {
         if (child.isMesh) {
-            child.material.color.set(0x87CEEB); // Ensure visible color
-            child.material.emissive.set(0x000000); // Add some emissive light
+            child.material.color.set(0xffffff); // Ensure visible color
+            child.material.emissive.set(0x00000); // Add some emissive light
             child.castShadow = true;
             child.receiveShadow = true;
         }
@@ -50,7 +54,7 @@ loader.load('/sigil/sigil.glb', function (gltf) {
     console.error(error);
 });
 
-camera.position.set(0, 0, 500);
+camera.position.set(0, 0, 450);
 
 const controls = new OrbitControls(camera, renderer.domElement);
 controls.update();
